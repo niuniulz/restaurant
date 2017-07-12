9bef5f0da7cdf5a67e7b1ab801bbdbfb666bfac4
牛牛 <553751688@qq.com>
2017-07-12 14:58:26 +0800

    创建项目

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
+
+import 'common/stylus/index.styl';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+// extend传入的参数是挂载的App.vue组件
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+// map方法配置路由
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+});
+
+router.start(app, '#app');
+
+router.go('/goods');
